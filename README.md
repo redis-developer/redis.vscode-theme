ce2a1a4f02b9327eae8760752c87d63101cfafa4
Will Johnston <will-github@wwj.dev>
2025-02-21 10:30:52 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# redis.vscode-theme
+
+This is the official VS Code theme for Redis, using the brand colors.
+
+![Redis VS Code theme screenshot](./doc/screenshot1.png)
+
+## Usage
+
+This isn't published as a VS Code extension, so to use it you will need to clone the repository and install it yourself.
+
+```bash
+git clone https://github.com/redis-developer/redis.vscode-theme.git
+```
+
+### Requirements
+
+1. [VS Code](https://code.visualstudio.com/)
+2. [bun](https://bun.sh/)
+
+### Deployment
+
+```
+bun run deploy
+```
+
+### Modifications
+
+All the colors are in `colors.js` and the configuration is in `index.js`
+
+### Contributions
+
+PRs are welcomed.
---
+++ colors.js
@@ -0,0 +1,52 @@
+const colors = {
+  bg: {
+    0: { light: "#e4dcd4", dark: "#091a23" },
+    1: { light: "#f6f2ee", dark: "#163341" },
+    2: { light: "#dbd1dd", dark: "#2D4754" },
+    3: { light: "#d3c7bb", dark: "#5C707A" },
+    4: { light: "#aab0ad", dark: "#8A99A0" },
+  },
+  fg: {
+    0: { light: "#302b5d", dark: "#B9C2C6" },
+    1: { light: "#3d2b5a", dark: "#D9D9D9" },
+    2: { light: "#643f61", dark: "#E9E9E9" },
+    3: { light: "#824d5b", dark: "#F3F3F3" },
+  },
+  black: {
+    default: { light: "#352c24", dark: "#282828" },
+    bright: { light: "#534c45", dark: "#484848" },
+    dim: { light: "#2d251f", dark: "#222222" },
+  },
+  blue: {
+    default: { light: "#2848a9", dark: "#3dd6f5" },
+    bright: { light: "#4863b6", dark: "#5adcf6" },
+    dim: { light: "#223d90", dark: "#11cdf3" },
+  },
+  comment: { light: "#837a72", dark: "#5c707a" },
+  magenta: {
+    default: { light: "#6e33ce", dark: "#c795e3" },
+    bright: { light: "#5e2baf", dark: "#cfa5e7" },
+    dim: { light: "#8452d5", dark: "#af68d7" },
+  },
+  red: {
+    default: { light: "#a5222f", dark: "#ff4438" },
+    bright: { light: "#b3434e", dark: "#ff6056" },
+    dim: { light: "#8c1d28", dark: "#ff1809" },
+  },
+  sel: {
+    0: { light: "#e7d2be", dark: "#e6e6e6" },
+    1: { light: "#a4c1c2", dark: "#d1d3d4" },
+  },
+  white: {
+    default: { light: "#f2e9e1", dark: "#f8f8f8" },
+    dim: { light: "#f4ece6", dark: "#d3d3d3" },
+    bright: { light: "#cec6bf", dark: "#f9f9f9" },
+  },
+  yellow: {
+    default: { light: "#ac5402", dark: "#ebff38" },
+    bright: { light: "#b86e28", dark: "#eeff56" },
+    dim: { light: "#924702", dark: "#e6ff09" },
+  },
+};
+
+export default colors;
